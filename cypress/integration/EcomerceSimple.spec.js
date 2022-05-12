539b514d2aa4c7fda5d2780a7d5035ae2e3489a0
franklinmeerga <62896416+franklinmeerga@users.noreply.github.com>
2022-05-12 18:57:06 -0300

    I found the mistake in the code

--- cypress/integration/EcomerceSimple.spec.js
+++ cypress/integration/EcomerceSimple.spec.js
@@ -1,6 +1,6 @@
 /// <reference types="cypress" />
 
-const { curryRight } = require("cypress/types/lodash");
+
 
 describe('fazer uma compra de um produto x', () => {
     // comando para visitar o site
@@ -8,41 +8,41 @@ describe('fazer uma compra de um produto x', () => {
         cy.visit('http://automationpractice.com/index.php')
     })
     
-    // it ('fazer login', () => {
-    //     // clicamos no botgão de login
-    //     cy.get('.login').click();
-    //     cy.get('#email_create').type('franklinmeerga@ufps.edu.co');
+    it ('fazer login', () => {
+        // clicamos no botgão de login
+        cy.get('.login').click();
+        cy.get('#email_create').type('franklinmeerga@ufps.edu.co');
         
-    //     // vamos escutar o post 200 que ele faz depois que ele clica no butão
+        // vamos escutar o post 200 que ele faz depois que ele clica no butão
         
-    //     // routing
-    //     // star do server com cy.server() mas no cypress 6.0 é cy.intercept()
-    //     // criar uma rota com cy.route()
-    //     // atribuir uma rota a um alias (as()) e fazer uma validação
-    //     // esperar com cy.wait()
+        // routing
+        // star do server com cy.server() mas no cypress 6.0 é cy.intercept()
+        // criar uma rota com cy.route()
+        // atribuir uma rota a um alias (as()) e fazer uma validação
+        // esperar com cy.wait()
 
-    //     //cy.intercept('**/index.php').as('postemail');
+        //cy.intercept('**/index.php').as('postemail');
 
 
-    //     cy.get('#SubmitCreate > span').click();
+        cy.get('#SubmitCreate > span').click();
 
    
-    //     cy.get('#customer_lastname').type('acevedo');
-    //     cy.get('#passwd').type('12345');
-    //     cy.get('#days').select('1');
-    //     cy.get('#months').select('3');
-    //     cy.get('#years').select('1992');
+        cy.get('#customer_lastname').type('acevedo');
+        cy.get('#passwd').type('12345');
+        cy.get('#days').select('1');
+        cy.get('#months').select('3');
+        cy.get('#years').select('1992');
 
 
-    //     cy.get('#firstname').type('franklin');
-    //     cy.get('#lastname').type("acevedo");
-    //     cy.get('#address1').type('abel dos santos 99');
-    //     cy.get('#city').type('são paulo');
-    //     cy.get('#id_state').select('Alabama');
-    //     cy.get('#postcode').type('12345');
-    //     cy.get('#id_country').select('United States');
-    //     cy.get('#phone_mobile').type('1234567801');
-    //     cy.get('#alias').type('sei laaa');
+        cy.get('#firstname').type('franklin');
+        cy.get('#lastname').type("acevedo");
+        cy.get('#address1').type('abel dos santos 99');
+        cy.get('#city').type('são paulo');
+        cy.get('#id_state').select('Alabama');
+        cy.get('#postcode').type('12345');
+        cy.get('#id_country').select('United States');
+        cy.get('#phone_mobile').type('1234567801');
+        cy.get('#alias').type('sei laaa');
 
-    // })
+    })
 })
